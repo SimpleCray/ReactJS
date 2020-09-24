242e393819383a093bb2a1002ee5c493cde79d53
SimpleCray <54101094+SimpleCray@users.noreply.github.com>
2020-09-24 20:52:29 +0700

    remove some unused part of homepage.js

--- demo_redux(class)/src/pages/HomePage.js
+++ demo_redux(class)/src/pages/HomePage.js
@@ -1,71 +1,10 @@
-// import React from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { addNewHobby, setActiveHobby } from '../actions/HobbyActions';
-// import HobbyList from '../components/Home/HobbyList';
-
-// HomePage.propTypes = {
-
-// };
-
-// const randomNumber = () => {
-//     return 1000 + Math.trunc(Math.random()*9000);
-// }
-
-// function HomePage(props){
-//     //useSelector is the replacement for mapStateToProps to use state in redux store (can use in functions only)
-//     const hobbyList = useSelector(state => state.hobby.list);//get from root reducer
-//     const activeId = useSelector(state => state.hobby.activeId);//get from root reducer
-
-//     const dispatch = useDispatch();
-
-//     console.log(hobbyList)
-//     console.log(activeId)
-//     const handleAddHobbyClick = ()=>{
-//         const newId = randomNumber()
-//         //Random a hobby object: id + title
-//         const newHobby ={
-//             id: newId,
-//             title: `Hobby ${newId}`,
-//         }
-
-//         //Dispatch action to add a new state to redux store
-//         const action = addNewHobby(newHobby);
-//         dispatch(action);
-//     }
-
-//     const handleHobbyClick=(hobby)=>{
-//         const action = setActiveHobby(hobby);
-//         dispatch(action);
-//     }
-
-//     return(
-//         <div className="home-page">
-//             <h1>Redux Hook</h1>
-//             <button onClick={handleAddHobbyClick}>Random hobby</button>
-//             <HobbyList
-//                 hobbyList={hobbyList}
-//                 activeId={activeId}
-//                 onHobbyClick={handleHobbyClick}
-//             />
-//         </div>
-//     )
-// }
-// export default HomePage;
-
 import React, { Component } from "react";
 import { addNewHobby, setActiveHobby } from "../actions/HobbyActions";
 import HobbyList from "../components/Home/HobbyList";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import autoBind from "react-autobind";
 
 class HomePage extends Component {
-  constructor(props, context) {
-    super(props, context);
-    autoBind(this);
-  }
-
-  //   randomNumber = () => (1000 + Math.trunc(Math.random() * 9000)
   handleAddHobbyClick = () => {
     const newId = 1000 + Math.trunc(Math.random() * 9000);
     //Random a hobby object: id + title
